2075565ed9936b1282ed380499d9a08413239f42
Jim Nelson <jim@midnightfreddie.com>
2016-07-09 09:55:21 -0500

    Minor changes, no success so far

--- NewWinVM.ps1
+++ NewWinVM.ps1
@@ -1,7 +1,18 @@
-
+param (
+    # $WimFile = "C:\temp\deleteme\sources\install.wim"
+    $WimFile = "D:\sources\install.wim"
+)
 
 # From https://github.com/Microsoft/Virtualization-Documentation
 # Specifically https://github.com/Microsoft/Virtualization-Documentation/tree/master/hyperv-tools/Convert-WindowsImage
 Import-Module Convert-WindowsImage
 
-Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath "D:\sources\install.wim" -SizeBytes 127GB -DiskLayout BIOS -ExpandOnNativeBoot:$false -Edition Standard
+Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath $WimFile -SizeBytes 127GB -DiskLayout BIOS -ExpandOnNativeBoot:$false -Edition Standard
+
+# Got this error, unsure yet if vhd is good
+# Convert-WindowsImage : The variable '$systemDrive' cannot be retrieved because it has not been set.
+# At C:\Users\Jim.AD\Documents\git\MidFredVmPlay\NewWinVM.ps1:10 char:1
+# + Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath $WimFile - ...
+# + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     + CategoryInfo          : NotSpecified: (:) [Write-Error], WriteErrorException
+#     + FullyQualifiedErrorId : Microsoft.PowerShell.Commands.WriteErrorException,Convert-WindowsImage
