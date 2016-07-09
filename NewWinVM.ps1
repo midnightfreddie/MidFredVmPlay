373cd540f846d770676ac10eda2cb6e48e2d74c2
Jim Nelson <jim@midnightfreddie.com>
2016-07-09 12:23:13 -0500

    Working with my patched Convert-WindowsImage Issue #335 fixed with pull request #336

--- NewWinVM.ps1
+++ NewWinVM.ps1
@@ -10,11 +10,14 @@ function New-MfVhd {
     )
     # From https://github.com/Microsoft/Virtualization-Documentation
     # Specifically https://github.com/Microsoft/Virtualization-Documentation/tree/master/hyperv-tools/Convert-WindowsImage
-    Import-Module Convert-WindowsImage
+    # Import-Module Convert-WindowsImage
+    # Now using my own fork to fix the -DiskLayout BIOS problem
+    Import-Module C:\Users\Jim.AD\Documents\git\Virtualization-Documentation\hyperv-tools\Convert-WindowsImage -Force
 
     # NOTE: I kept getting the following error; it turns out my iso file was corrupt. Its SHA1 signature didn't match MSDN
     #   Convert-WindowsImage : The file or directory is corrupted and unreadable. (Exception from HRESULT: 0x80070570)
     # Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath $WimFile -SizeBytes 127GB -DiskLayout BIOS -ExpandOnNativeBoot:$false -Edition Standard
+    Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath $WimFile -SizeBytes 127GB -DiskLayout BIOS -ExpandOnNativeBoot:$false -Edition Standard
 
     # Got this error, looks like it means boot wasn't set up
     # Convert-WindowsImage : The variable '$systemDrive' cannot be retrieved because it has not been set.
@@ -24,15 +27,15 @@ function New-MfVhd {
     #     + CategoryInfo          : NotSpecified: (:) [Write-Error], WriteErrorException
     #     + FullyQualifiedErrorId : Microsoft.PowerShell.Commands.WriteErrorException,Convert-WindowsImage
 
-    Convert-WindowsImage -VhdPath c:\vm\Win2012r2.vhdx -SourcePath $WimFile -SizeBytes 127GB -DiskLayout UEFI -ExpandOnNativeBoot:$false -Edition Standard
+    # Convert-WindowsImage -VhdPath c:\vm\Win2012r2.vhdx -SourcePath $WimFile -SizeBytes 127GB -DiskLayout UEFI -ExpandOnNativeBoot:$false -Edition Standard
 
 }
 
 # Using for reference: http://www.tomsitpro.com/articles/hyper-v-powershell-cmdlets,2-779.html
 function New-MfVm {
-    New-VM -Name Win2012 -VHDPath C:\vm\Win2012r2.vhdx -MemoryStartupBytes 1024mb
+    New-VM -Name Win2012 -VHDPath C:\vm\9600.17415.amd64fre.winblue_r4.141028-1500_Server_ServerStandard_en-US.vhd -MemoryStartupBytes 1024mb
     Get-VM win2012 | Get-VMNetworkAdapter | Connect-VMNetworkAdapter -SwitchName Internal-ICS-NAT
 }
 
-New-MfVhd $WimFile
-New-MfVm
+# New-MfVhd $WimFile
+# New-MfVm
