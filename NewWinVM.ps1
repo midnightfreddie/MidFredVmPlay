cbc1c4305cfbf1c9e26469744e825d0d63705e1f
Jim Nelson <jim@midnightfreddie.com>
2016-07-09 10:12:04 -0500

    VM creation, image won't boot

--- NewWinVM.ps1
+++ NewWinVM.ps1
@@ -3,16 +3,30 @@ param (
     $WimFile = "D:\sources\install.wim"
 )
 
-# From https://github.com/Microsoft/Virtualization-Documentation
-# Specifically https://github.com/Microsoft/Virtualization-Documentation/tree/master/hyperv-tools/Convert-WindowsImage
-Import-Module Convert-WindowsImage
+function New-MfVhd {
+    param (
+        [Param(Mandatory=$true)]
+        $WimFile
+    )
+    # From https://github.com/Microsoft/Virtualization-Documentation
+    # Specifically https://github.com/Microsoft/Virtualization-Documentation/tree/master/hyperv-tools/Convert-WindowsImage
+    Import-Module Convert-WindowsImage
 
-Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath $WimFile -SizeBytes 127GB -DiskLayout BIOS -ExpandOnNativeBoot:$false -Edition Standard
+    Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath $WimFile -SizeBytes 127GB -DiskLayout BIOS -ExpandOnNativeBoot:$false -Edition Standard
 
-# Got this error, unsure yet if vhd is good
-# Convert-WindowsImage : The variable '$systemDrive' cannot be retrieved because it has not been set.
-# At C:\Users\Jim.AD\Documents\git\MidFredVmPlay\NewWinVM.ps1:10 char:1
-# + Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath $WimFile - ...
-# + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#     + CategoryInfo          : NotSpecified: (:) [Write-Error], WriteErrorException
-#     + FullyQualifiedErrorId : Microsoft.PowerShell.Commands.WriteErrorException,Convert-WindowsImage
+    # Got this error, looks like it means boot wasn't set up
+    # Convert-WindowsImage : The variable '$systemDrive' cannot be retrieved because it has not been set.
+    # At C:\Users\Jim.AD\Documents\git\MidFredVmPlay\NewWinVM.ps1:10 char:1
+    # + Convert-WindowsImage -WorkingDirectory "C:\vm" -SourcePath $WimFile - ...
+    # + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #     + CategoryInfo          : NotSpecified: (:) [Write-Error], WriteErrorException
+    #     + FullyQualifiedErrorId : Microsoft.PowerShell.Commands.WriteErrorException,Convert-WindowsImage
+}
+
+function New-MfVm {
+    New-VM -Name Win2012 -VHDPath C:\vm\Win2012r2.vhd -MemoryStartupBytes 1024mb
+    Get-VM win2012 | Get-VMNetworkAdapter | Connect-VMNetworkAdapter -SwitchName Internal-ICS-NAT
+}
+
+# New-MfVhd $WimFile
+# New-MfVm
