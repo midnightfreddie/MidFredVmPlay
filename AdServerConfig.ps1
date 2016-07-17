739306b61ee16bde05467f4408274183585f25bc
Jim Nelson <jim@midnightfreddie.com>
2016-07-17 08:28:13 -0500

    Added AdServerConfig with Script resource for IPs

---
+++ AdServerConfig.ps1
@@ -0,0 +1,36 @@
+configuration DsController {
+    param (
+        $ComputerName = "localhost",
+        $IPv4Address = "192.168.137.245",
+        $IPv6Address = "fd43:4834:bd2d::245"
+    )
+    Node $ComputerName {
+        LocalConfigurationManager {
+            ActionAfterReboot = 'ContinueConfiguration'
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+        # FIXME: New-NetIPAddress will fail if the address already exists
+        Script NetworkSettings {
+            SetScript = ({
+                New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4 -IPAddress "{0}" -PrefixLength 24 -DefaultGateway ("{0}" -replace "\.\d+$", ".1")
+                New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv6 -IPAddress "{1}" -PrefixLength 64
+                Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses 192.168.1.3
+            }) -f $IPv4Address, $IPv6Address
+            TestScript = { $false }
+            GetScript = { @{} }
+        }
+        # WindowsFeature ADDSInstall {
+        #     Ensure = "Present"
+        #     Name = "AD-Domain-Services"
+        # }
+        WindowsFeature DHCP {
+            Ensure = "Present"
+            Name = "DHCP"
+        }
+        WindowsFeature DNS {
+            Ensure = "Present"
+            Name = "DNS"
+        }
+    }
+}
