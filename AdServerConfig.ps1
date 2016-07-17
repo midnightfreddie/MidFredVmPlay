f49e5ff5d6a4cc793423bafd6baf62b1d91fab3a
Jim Nelson <jim@midnightfreddie.com>
2016-07-17 11:54:17 -0500

    Aborting attempt at xIPAddress resource

--- AdServerConfig.ps1
+++ AdServerConfig.ps1
@@ -1,25 +1,42 @@
 configuration DsController {
     param (
-        $ComputerName = "localhost",
+        $ComputerName = "freddievm1",
         $IPv4Address = "192.168.137.245",
         $IPv6Address = "fd43:4834:bd2d::245"
     )
+    Import-DscResource -Module xNetworking
     Node $ComputerName {
         LocalConfigurationManager {
             ActionAfterReboot = 'ContinueConfiguration'
             ConfigurationMode = 'ApplyOnly'
             RebootNodeIfNeeded = $true
         }
-        # FIXME: New-NetIPAddress will fail if the address already exists
-        Script NetworkSettings {
-            SetScript = ({
-                New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4 -IPAddress "{0}" -PrefixLength 24 -DefaultGateway ("{0}" -replace "\.\d+$", ".1")
-                New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv6 -IPAddress "{1}" -PrefixLength 64
-                Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses 192.168.1.3
-            }) -f $IPv4Address, $IPv6Address
-            TestScript = { $false }
-            GetScript = { @{} }
+
+        # # FIXME: New-NetIPAddress will fail if the address already exists
+        # Script NetworkSettings {
+        #     SetScript = ({
+        #         New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4 -IPAddress "{0}" -PrefixLength 24 -DefaultGateway ("{0}" -replace "\.\d+$", ".1")
+        #         New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv6 -IPAddress "{1}" -PrefixLength 64
+        #         Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses 192.168.1.3
+        #     }) -f $IPv4Address, $IPv6Address
+        #     TestScript = { $false }
+        #     GetScript = { @{} }
+        # }
+
+        xIPAddress IPv4Address {
+            IPAddress      = $IPv4Address
+            InterfaceAlias = "Ethernet"
+            SubnetMask     = 24
+            AddressFamily  = "IPV4"
         }
+
+        xIPAddress IPv6Address {
+            IPAddress      = $IPv6Address
+            InterfaceAlias = "Ethernet"
+            SubnetMask     = 64
+            AddressFamily  = "IPV6"
+        }
+
         # WindowsFeature ADDSInstall {
         #     Ensure = "Present"
         #     Name = "AD-Domain-Services"
