1554d15b90c32cfafea45aa132546bbdc2eca091
Jim Nelson <jim@midnightfreddie.com>
2016-07-17 12:32:28 -0500

    Removed xIPAddress and fixed errors when rerun

--- AdServerConfig.ps1
+++ AdServerConfig.ps1
@@ -4,7 +4,6 @@ configuration DsController {
         $IPv4Address = "192.168.137.245",
         $IPv6Address = "fd43:4834:bd2d::245"
     )
-    Import-DscResource -Module xNetworking
     Node $ComputerName {
         LocalConfigurationManager {
             ActionAfterReboot = 'ContinueConfiguration'
@@ -12,29 +11,29 @@ configuration DsController {
             RebootNodeIfNeeded = $true
         }
 
-        # # FIXME: New-NetIPAddress will fail if the address already exists
-        # Script NetworkSettings {
-        #     SetScript = ({
-        #         New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4 -IPAddress "{0}" -PrefixLength 24 -DefaultGateway ("{0}" -replace "\.\d+$", ".1")
-        #         New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv6 -IPAddress "{1}" -PrefixLength 64
-        #         Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses 192.168.1.3
-        #     }) -f $IPv4Address, $IPv6Address
-        #     TestScript = { $false }
-        #     GetScript = { @{} }
-        # }
-
-        xIPAddress IPv4Address {
-            IPAddress      = $IPv4Address
-            InterfaceAlias = "Ethernet"
-            SubnetMask     = 24
-            AddressFamily  = "IPV4"
+        Script IPv4Address {
+            SetScript = ({
+                New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4 -IPAddress "{0}" -PrefixLength 24 -DefaultGateway ("{0}" -replace "\.\d+$", ".1")
+                Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses 192.168.1.3
+            }) -f $IPv4Address
+            TestScript = ({
+                "{0}" -in (Get-NetIPAddress -InterfaceAlias Ethernet | Select-Object -ExpandProperty IPAddress)
+            }) -f $IPv4Address
+            GetScript = {
+                @{ Result = (Get-NetIPAddress -InterfaceAlias Ethernet | Select-Object -ExpandProperty IPAddress) -join ", " }
+            }
         }
 
-        xIPAddress IPv6Address {
-            IPAddress      = $IPv6Address
-            InterfaceAlias = "Ethernet"
-            SubnetMask     = 64
-            AddressFamily  = "IPV6"
+        Script IPv6Address {
+            SetScript = ({
+                New-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv6 -IPAddress "{0}" -PrefixLength 64
+            }) -f $IPv6Address
+            TestScript = ({
+                "{0}" -in (Get-NetIPAddress -InterfaceAlias Ethernet | Select-Object -ExpandProperty IPAddress)
+            }) -f $IPv6Address
+            GetScript = {
+                @{ Result = (Get-NetIPAddress -InterfaceAlias Ethernet | Select-Object -ExpandProperty IPAddress) -join ", " }
+            }
         }
 
         # WindowsFeature ADDSInstall {
